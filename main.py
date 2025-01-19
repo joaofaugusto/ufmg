3c7d1059c3e149c0fd47a9357905ac44f56ba9a4
joaofaugusto <emailjoao111@gmail.com>
2025-01-18 22:21:13 -0300

    adicionando comentarios e corrigindo o arquivo de caminhos

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ def ler_cidades_de_arquivo(nome_arquivo):
     try:
         lista_cidades = []
         i = 0
-        with open(nome_arquivo, "r", encoding='utf-8') as arquivo:
-            for linha in arquivo:
-                cidade = (linha.strip(), i)
-                lista_cidades.append(cidade)
-                i+=1
+        with open(nome_arquivo, "r", encoding='utf-8') as arquivo: # Abre o arquivo para leitura
+            for linha in arquivo: # Lê cada linha do arquivo
+                cidade = (linha.strip(), i) # Remove espaços em branco e atribui um índice para a cidade
+                lista_cidades.append(cidade) # Adiciona a cidade à lista de cidades
+                i+=1 # Incrementa o índice
         return lista_cidades
     except FileNotFoundError:
         print(f"Arquivo '{nome_arquivo}' não encontrado.")
         return []
 
-def ler_mapa(nome_arquivo, cidades_lidas, quantidade_cidades):
-    mapa = np.zeros((quantidade_cidades, quantidade_cidades))
+def ler_mapa(nome_arquivo, cidades_lidas, quantidade_cidades): 
+    mapa = np.zeros((quantidade_cidades, quantidade_cidades)) # Cria uma matriz de zeros com a quantidade de cidades
     try:
-        with open(nome_arquivo, "r", encoding='utf-8') as arquivo:
-            linhas = arquivo.readlines()
-            for linha in linhas:
-                cidade_info = linha.strip().split(",")
+        with open(nome_arquivo, "r", encoding='utf-8') as arquivo: # Abre o arquivo para leitura
+            linhas = arquivo.readlines() # Lê todas as linhas do arquivo
+            for linha in linhas: # Para cada linha do arquivo
+                cidade_info = linha.strip().split(",") # Remove espaços em branco e divide a linha em uma lista
                 try:
-                    if len(cidade_info) == 3:
-                        origem, destino, custo = cidade_info
-                        mapa[cidades_lidas[origem]][cidades_lidas[destino]] = int(custo)
-                        mapa[cidades_lidas[destino]][cidades_lidas[origem]] = int(custo)
+                    if len(cidade_info) == 3: # Se a lista tiver 3 elementos
+                        origem, destino, custo = cidade_info # Atribui os elementos a origem, destino e custo
+                        mapa[cidades_lidas[origem]][cidades_lidas[destino]] = int(custo) # Adiciona o custo à matriz
+                        mapa[cidades_lidas[destino]][cidades_lidas[origem]] = int(custo) # Adiciona o custo à matriz
                 except:
                     print("")
         return mapa
@@ -57,17 +57,17 @@ def vizinho_mais_proximo(indices_a_percorrer, matriz_distancia):
         distancia_minima = float('inf')
         
         # Neste loop, para cada cidade não visitada, pegamos a distância entre a cidade atual e a cidade candidata (cidade). Se essa distância for menor que distancia_minima e maior que 0, atualizamos distancia_minima e cidade_mais_proxima. distance > 0 evita escolher um caminho inválido.
-        for cidade in nao_visitado:
-            distancia = matriz_distancia[cidade_atual][cidade]
-            if distancia < distancia_minima and distancia > 0:
-                distancia_minima = distancia
-                cidade_mais_proxima = cidade
+        for cidade in nao_visitado: 
+            distancia = matriz_distancia[cidade_atual][cidade] # Calcula a distância entre a cidade atual e a cidade candidata
+            if distancia < distancia_minima and distancia > 0: # Se a distância for menor que a distância mínima e maior que 0
+                distancia_minima = distancia # Atualiza a distância mínima
+                cidade_mais_proxima = cidade # Atualiza a cidade mais próxima
         
 
         # Se nenhuma cidade foi encontrada, escolhemos a primeira cidade não visitada e atribuímos a distância correspondente.
         if cidade_mais_proxima is None:
-            cidade_mais_proxima = nao_visitado[0]
-            distancia_minima = matriz_distancia[cidade_atual][cidade_mais_proxima]
+            cidade_mais_proxima = nao_visitado[0] # Atribui a primeira cidade não visitada
+            distancia_minima = matriz_distancia[cidade_atual][cidade_mais_proxima] # Atribui a distância correspondente
             
         # Adicionamos a cidade encontrada à rota.    
         rota.append(cidade_mais_proxima)
@@ -78,11 +78,11 @@ def vizinho_mais_proximo(indices_a_percorrer, matriz_distancia):
         # Adicionamos a distância mínima ao custo total.
         custo_total += distancia_minima
     
-    return rota, custo_total
+    return rota, custo_total # Retorna a rota e o custo total
 
 def caminho_mais_curto(cidades_obrigatorias, dicionario_cidades, matriz_distancia):  
     # Convertendo os nomes das cidades de cidades_obrigatorias para seus índices correspondentes na matriz_distancia. Isso facilita a busca na matriz de distâncias, pois as funções trabalham com índices numéricos
-    indices_obrigatorios = [dicionario_cidades[city] for city in cidades_obrigatorias]
+    indices_obrigatorios = [dicionario_cidades[cidade] for cidade in cidades_obrigatorias]
     
     melhor_rota = None # armazenará a melhor rota encontrada.
     custo_minimo = float('inf') # armazenará o menor custo encontrado, inicializado com infinito (float('inf')), garantindo que qualquer rota válida será menor.
@@ -98,8 +98,7 @@ def caminho_mais_curto(cidades_obrigatorias, dicionario_cidades, matriz_distanci
         rota, custo = vizinho_mais_proximo(indices_rotacionados, matriz_distancia)
         
 
-        # Se o custo da nova rota (cost) for menor que min_cost, atualizamos
-
+        # Se o custo da nova rota (custo) for menor que min_cost, atualizamos
         if custo < custo_minimo:
             custo_minimo = custo # menor custo encontrado.
             melhor_rota = rota # melhor rota correspondente.
@@ -116,25 +115,20 @@ def main():
         "Neuquen", "Quilmes"
     ]
     
-    print("Lendo as cidades dos arquivos...")
     lista_cidades = ler_cidades_de_arquivo("Cidades.txt")
-    
     if not lista_cidades:
         print("Nenhuma cidade foi encontrada no arquivo.")
         return
     
-    print(f"\nEncontrado {len(lista_cidades)} cidades no arquivo.")
-    
     # Converte a lista de cidades em um dicionário de mapeamento {nome: índice}, facilitando buscas rápidas.
     dicionario_cidades = {cidade[0]: cidade[1] for cidade in lista_cidades}
     
-    print("\nLendo o arquivo Caminho.txt...")
     # A função ler_mapa precisa converter os dados corretamente para formar a distance_matrix
-    matriz_distancia = ler_mapa("Caminho.txt", dicionario_cidades, len(lista_cidades))
-    
+    matriz_distancia = ler_mapa("Caminho.txt", dicionario_cidades, len(lista_cidades)) # Lê o mapa e retorna a matriz de distâncias
+     
     try:
         # A função caminho_mais_curto encontra a melhor rota testando diferentes cidades como ponto de partida. Retorna a melhor sequência de cidades e o custo total da viagem.
-        melhor_rota, custo_minimo = caminho_mais_curto(cidades_obrigatorias, dicionario_cidades, matriz_distancia)
+        melhor_rota, custo_minimo = caminho_mais_curto(cidades_obrigatorias, dicionario_cidades, matriz_distancia) # Encontra a melhor rota e o custo total
         
         # Como caminho_mais_curto retorna os índices das cidades, precisamos converter os índices de volta para os nomes das cidades.
         # melhor_rota = [2, 0, 3, 1] 
@@ -144,7 +138,7 @@ def main():
         rotas = [indice_ate_cidade[i] for i in melhor_rota]
         
         # Resultados
-        print("\nRota encontratada:")
+        print("\nRota encontrada:")
         for i, cidade in enumerate(rotas, 1):
             print(f"{i}. {cidade}")
         print(f"\nCusto total: {custo_minimo}")
